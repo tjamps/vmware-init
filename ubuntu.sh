85b4086d9df8cee61539c933f024fd442ca0d0e1
Rémi T'JAMPENS <remi.tjampens@gmail.com>
2014-12-07 22:05:29 +0100

    Networking functions.

--- ubuntu.sh
+++ ubuntu.sh
@@ -7,6 +7,15 @@ function exit_failure {
     exit 1
 }
 
+function print_info {
+    echo -e "\e[1;37m+ $1\e[0m"
+}
+
+function print_variable_info {
+    echo -ne "\e[1;37m+ $1 : \e[0m"
+    echo -e  "\e[36m $2\e[0m"
+}
+
 function is_os_ubuntu {
     command -v lsb_release > /dev/null 2>&1 || return 1
     (lsb_release -a | grep -i ubuntu) > /dev/null 2>&1 && return 0
@@ -19,6 +28,43 @@ function is_user_root {
     return 1
 }
 
+function is_vmware {
+    (dmidecode -s system-product-name | grep -i vmware) > /dev/null 2>&1 && return 0
+    return 1
+}
+
+function get_current_ip {
+    ip -o -4 addr show eth0 | awk '{ print $4; }' | cut -d/ -f1
+}
+
+function set_static_ip {
+    
+    local current_ip=$(get_current_ip)
+    local current_gw=$(echo $current_ip | cut -d. -f1-3).2
+    
+    mv /etc/network/interfaces /etc/network/interfaces.bak
+    cat << EOF > /etc/network/interfaces
+# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+
+# The loopback network interface
+auto lo
+iface lo inet loopback
+
+# The primary network interface
+auto eth0
+iface eth0 inet static
+    address $current_ip
+    netmask 255.255.255.0
+    gateway $current_gw
+    dns-nameservers $current_gw
+EOF
+}
+
+function restart_networking {
+    ifdown eth0 && ifup eth1
+}
+
 if ! is_os_ubuntu; then
     exit_failure "This script is only intended to be run on Ubuntu GNU/Linux."
 fi
@@ -27,3 +73,13 @@ if ! is_user_root; then
     exit_failure "You must be root to execute this script (sudo -i)."
 fi
 
+if ! is_vmware; then
+    exit_failure "This script is only intended to be run on VMware virtual machines."
+fi
+
+# Fix IP address
+print_info "Setting static IP address."
+print_variable_info "Detected IP address" get_current_ip
+
+# Reload networking
+# 
