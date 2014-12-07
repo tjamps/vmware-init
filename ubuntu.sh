f53c33e8e550a879ffee29f4fe0d02ea58c73b05
Rémi T'JAMPENS <remi.tjampens@gmail.com>
2014-12-07 22:15:35 +0100

    Static IP address setup.

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
 function exit_failure {
-    if [ "$1" ]; then
-        echo -e "\e[31mError: $1\e[0m"
-    fi
+    # Print red $1
+    echo -e "\e[31mError: $1\e[0m"
     exit 1
 }
 
 function print_info {
+    # Print bold white $1
     echo -e "\e[1;37m+ $1\e[0m"
 }
 
 function print_variable_info {
+    # $1 : Label to display
+    # $2 : Variable to display
     echo -ne "\e[1;37m+ $1 : \e[0m"
     echo -e  "\e[36m $2\e[0m"
 }
 
 function is_os_ubuntu {
+    # Check if command exists.
     command -v lsb_release > /dev/null 2>&1 || return 1
     (lsb_release -a | grep -i ubuntu) > /dev/null 2>&1 && return 0
     
@@ -38,8 +41,8 @@ function get_current_ip {
 }
 
 function set_static_ip {
-    
     local current_ip=$(get_current_ip)
+    # Search for default VMware gateway ;)
     local current_gw=$(echo $current_ip | cut -d. -f1-3).2
     
     mv /etc/network/interfaces /etc/network/interfaces.bak
@@ -62,8 +65,13 @@ EOF
 }
 
 function restart_networking {
-    ifdown eth0 && ifup eth1
+    ifdown eth0 && ifup eth1 && return 0
+    return 1
 }
+
+###############################################################################
+# MAIN
+###############################################################################
 
 if ! is_os_ubuntu; then
     exit_failure "This script is only intended to be run on Ubuntu GNU/Linux."
@@ -78,8 +86,16 @@ if ! is_vmware; then
 fi
 
 # Fix IP address
-print_info "Setting static IP address."
-print_variable_info "Detected IP address" get_current_ip
+print_info "Setting static IP address..."
 
-# Reload networking
-# 
+print_variable_info "Detected IP address" $(get_current_ip)
+
+set_static_ip
+
+print_info "Restart networking..."
+if ! restart_networking; then
+    exit_failure "Network could not be restarted."
+fi
+
+print_info "Static IP address set."
+
