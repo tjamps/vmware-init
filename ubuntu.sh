f38e628bafc20a5031955528c112c1f67c659e3f
Rémi T'JAMPENS <remi.tjampens@gmail.com>
2014-12-07 22:48:40 +0100

    VMware Tools installation.

--- ubuntu.sh
+++ ubuntu.sh
@@ -18,6 +18,9 @@ function print_variable_info {
     echo -e  "\e[36m $2\e[0m"
 }
 
+function pause {
+    read -r -n1 -s
+}
 function is_os_ubuntu {
     # Check if command exists.
     command -v lsb_release > /dev/null 2>&1 || return 1
@@ -46,7 +49,7 @@ function get_current_ip {
 function set_static_ip {
     local current_ip=$(get_current_ip)
     # Search for default VMware gateway ;)
-    local current_gw=$(echo $current_ip | cut -d. -f1-3).2
+    local current_gw=$(echo "$current_ip" | cut -d. -f1-3).2
     
     mv /etc/network/interfaces /etc/network/interfaces.bak
     cat << EOF > /etc/network/interfaces
@@ -72,8 +75,6 @@ function restart_networking {
     return 1
 }
 
-
-
 ###############################################################################
 # UPGRADE
 ###############################################################################
@@ -82,6 +83,29 @@ function upgrade_machine {
     apt-get upgrade -y > /dev/null 2>&1 || return 1
     return 0
 }
+
+###############################################################################
+# VMWARE TOOLS
+###############################################################################
+function install_vmware_tools_dependencies {
+    apt-get -y install build-essential linux-headers-"$(uname -r)" > /dev/null 2>&1 || return 1
+    return 0
+}
+
+function mount_vmware_tools_disc {
+    mount /dev/cdrom /media/cdrom > /dev/null 2>&1 && return 0
+    return 1
+}
+
+function extract_vmware_tools_archive {
+    tar xf /media/cdrom/VMwareTools*.tar.gz -C /tmp > /dev/null 2>&1 || return 1
+    return 0
+}
+
+function install_vmware_tools {
+    /tmp/vmware-tools-distrib/vmware-install.pl -d > "$1" 2>&1
+}
+
 
 ###############################################################################
 # MAIN
@@ -102,7 +126,7 @@ fi
 # Fix IP address
 print_info "Setting static IP address..."
 
-print_variable_info "Detected IP address" $(get_current_ip)
+print_variable_info "Detected IP address" "$(get_current_ip)"
 
 set_static_ip
 
@@ -122,3 +146,29 @@ fi
 print_info "Machine upgraded."
 
 
+# VMware tools
+print_info "Setting up VMware tools..."
+print_info "Installing dependencies..."
+if ! install_vmware_tools_dependencies; then
+    exit_failure "Could not install VMware Tools dependencies."
+fi
+print_info "Dependencies installed."
+print_info "You must now insert the VMware Tools installation disc. This can be done"
+print_info "by clicking \"Virtual Machine > Install VMware Tools...\" in the menu."
+print_info "Press any key when you're done."
+pause
+echo # For optics ;)
+
+if ! mount_vmware_tools_disc; then
+    exit_failure "VMware Tools disc could not be mounted."
+fi
+if ! extract_vmware_tools_archive; then
+    exit_failure "VMware Tools archive could not be extracted."
+fi
+print_info "Installation log is available at /root/vmware-tools-install.log"
+install_vmware_tools "/root/vmware-tools-install.log"
+
+
+print_info "Seems all good ! You can reboot now :)"
+
+exit 0
